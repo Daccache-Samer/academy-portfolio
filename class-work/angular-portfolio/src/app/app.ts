6fedd390d7ce2ec39f1b2eed1dc1dd2aa5e714d2
Daccache-Samer <samerdacc@gmail.com>
2026-08-09 15:45:33 +0300

    feat(session-11): add scroll-to-top button

--- class-work/angular-portfolio/src/app/app.ts
+++ class-work/angular-portfolio/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, HostListener, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
 
@@ -6,8 +6,18 @@ import { Navbar } from './navbar/navbar';
   selector: 'app-root',
   imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('angular-portfolio');
+  protected readonly showBackToTop = signal(false);
+
+  @HostListener('window:scroll')
+  onWindowScroll(): void {
+    this.showBackToTop.set(window.scrollY > 300);
+  }
+
+  scrollToTop(): void {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
